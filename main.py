226cbc2994a8c8a33db3e5651af641fcda840c17
jiwookki <jiwookki@gmail.com>
2021-12-09 18:30:09 +0800

    got the code
    copied and pasted from JiBotForDiscord and TeamiePostBot

--- main.py
+++ main.py
@@ -3,8 +3,39 @@
 
 from discord_webhook import DiscordWebhook, DiscordEmbed
 import time, logging, sys
+import scraping
 
 
 
+
+url="" # Insert Discord Webhook URL here
+
+
+
+
+
+def main():
+	scraper = scraping.CovidScraper()
+
+
+	while True:
+
+		discmessage = DiscordEmbed(title="Daily SG Covid-19 Update", color='ed2939')
+    
+        discmessage.set_footer(name='MohScraperBot by JiBot#7904')
+    
+        discmessage.set_timestamp()
+        discmessage.add_embed_field(name="Update", value=scraper.getcontents())
+
+    
+        webhook = DiscordWebhook(url=url, username = "COVID-19 Updates")
+        webhook.add_embed(discmessage)
+        response = webhook.execute()
+
+        scraper.quit()
+
+        time.sleep(86400)
+        
+        scraper.reopen_page()
 
 
--- scraping.py
+++ scraping.py
@@ -5,3 +5,37 @@
 import urllib, os, platform
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
+
+driver_options = webdriver.firefox.options.Options()
+driver_options.add_argument('--headless')
+driver_options.add_argument("--disable-gpu")
+
+
+class ScrapedPage():
+    def __init__(self, url):
+        self.url = url
+        self.browser = webdriver.Firefox(executable_path = execpath, options=driver_options)
+        self.browser.get(url)
+       #self.text = urllib.requests.urlopen(self.url).read()
+       #self.soup = BeautifulSoup(self.text, "lxml")
+    def quit(self):
+    	self.browser.quit()
+    def reopen_page(self):
+    	self.browser = webdriver.Firefox(executable_path = execpath, options=driver_options)
+        self.browser.get(url)
+
+class CovidScraper(ScrapedPage):
+    def __init__(self, url='https://www.gov.sg/features/covid-19'):
+        print("launch")
+        super().__init__(url)
+        print("super init")
+        self.covid_box = self.browser.find_element_by_css_selector("div.feature-intro.content")
+        print("got box")
+      # self.covidupdate = self.soup.find_all(class_="feature-intro__content")
+    def getcontents(self):
+        return self.covid_box.text
+
+
+
+    	
